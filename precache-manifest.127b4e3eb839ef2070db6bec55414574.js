bcdc128a93a44cad3221fe37b9a9d7d8127c05c0
Parag Rajhans <prajhan1@binghamton.edu>
2020-04-30 18:45:12 -0700

    Updates

---
+++ precache-manifest.127b4e3eb839ef2070db6bec55414574.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0174296865236a94fb5fd1a507d9671",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "29f0d394694b73c9c7c8",
+    "url": "/shopping-cart/static/css/main.b3845f6c.chunk.css"
+  },
+  {
+    "revision": "bbdf396a060f0b904391",
+    "url": "/shopping-cart/static/js/2.bf51374a.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/shopping-cart/static/js/2.bf51374a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29f0d394694b73c9c7c8",
+    "url": "/shopping-cart/static/js/main.8cdd6ecb.chunk.js"
+  },
+  {
+    "revision": "8e3dad8a73325e62f16c",
+    "url": "/shopping-cart/static/js/runtime-main.8e6a9ade.js"
+  },
+  {
+    "revision": "d5a5c0df21b2f341107d0e36215fe4cd",
+    "url": "/shopping-cart/static/media/no_items_found.d5a5c0df.jpg"
+  }
+]);
